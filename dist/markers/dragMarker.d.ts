7f83a96d4c989292810e388d0f83a34afcafa841
Candid Dauth <cdauth@cdauth.eu>
2021-02-26 23:56:06 +0100

    v1.0.6

---
+++ dist/markers/dragMarker.d.ts
@@ -0,0 +1,11 @@
+import { LatLngExpression, Map, MarkerOptions, Polyline } from "leaflet";
+import DraggableLinesHandler from "../handler";
+import DraggableLinesMarker from "./marker";
+export default class DraggableLinesDragMarker extends DraggableLinesMarker {
+    _idx: number | [number, number];
+    _removeOnClick: boolean;
+    constructor(draggable: DraggableLinesHandler, layer: Polyline, latlng: LatLngExpression, idx: number | [number, number], options: MarkerOptions, removeOnClick: boolean);
+    onAdd(map: Map): this;
+    getIdx(): number | [number, number];
+    handleClick(): void;
+}
