7b10dada37e21e7fc49c60b56f42ed7e331fe876
Candid Dauth <cdauth@cdauth.eu>
2023-12-26 01:08:25 +0100

    Fix injections in type declarations

--- src/injections.ts
+++ src/injections.ts
@@ -19,25 +19,3 @@ Polyline.prototype.setLatLngs = function(...args: any) {
     this.fire('draggableLines-setLatLngs');
     return result;
 };
-
-interface PolylineInfo {
-    dragMarkers: Marker[];
-    plusMarkers: Marker[];
-    zoomEndHandler: () => void;
-}
-
-declare module "leaflet" {
-    interface Polyline {
-        hasDraggableLinesRoutePoints: () => boolean;
-        getDraggableLinesRoutePoints: () => LatLng[] | undefined;
-        setDraggableLinesRoutePoints: (routePoints: LatLngExpression[] | undefined) => void;
-    }
-
-    interface PolylineOptions {
-        draggableLinesRoutePoints?: LatLngExpression[];
-    }
-
-    interface Polyline {
-        _draggableLines?: PolylineInfo;
-    }
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import dtsPlugin from 'vite-plugin-dts';
+import { appendFile, readFile } from "fs/promises";
 
 export default defineConfig({
 	plugins: [
 		cssInjectedByJsPlugin(),
-		dtsPlugin({ clearPureImport: false, rollupTypes: true })
+		dtsPlugin({
+			clearPureImport: false,
+			rollupTypes: true,
+			async afterBuild() {
+				// Due to https://github.com/microsoft/rushstack/issues/1709, our module augmentations are lost during
+				// the type rollup. As an ugly workaround, we simply append them here.
+				const filterFile = await readFile("./src/injections-types.mts");
+				await appendFile("./dist/L.DraggableLines.d.ts", filterFile);
+			},
+		})
 	],
 	build: {
 		sourcemap: true,
